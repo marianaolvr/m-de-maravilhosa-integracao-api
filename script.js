c2e21f3b3db805cf3a38200ea3c593f60c33cb3d
Mariana Oliveira <olvr.mariana@gmail.com>
2019-05-24 20:17:08 -0300

    Acrescentou o botão deletar para tirar itens da página e da API

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
+//GET
 const box = document.querySelector(".maravilhosas__box");
-
 
 fetch('http://localhost:5001/maravilhosas/')
 
-.then((response) =>{
-    return response.json();
-})
-.then((data) => {
-    data.content.forEach(mulher =>{
-
-        let perfil = document.createElement('div');
-        perfil.setAttribute('class', 'maravilhosas__perfil');
-        box.appendChild(perfil);
-        
-        let link = document.createElement('a');
-        link.href = '#!';
-        perfil.appendChild(link);
-
-        // let imagem = document.createElement('img');
-        // imagem.setAttribute('class', 'img-responsive');
-        // imagem.src = '#!';
-        // link.appendChild(imagem);
-
-        let nome = document.createElement('p');
-        nome.innerHTML = mulher.title;
-        link.appendChild(nome);
-
-        let botao = document.createElement('button');
-        link.appendChild(botao);
-        botao.textContent = "Deletar"
-
-        let image = document.createElement('img');
-        image.setAttribute('class', 'img-responsive');
-        nome.appendChild(image);
-
-        if(mulher.metadata && mulher.metadata.image){
-            image.setAttribute('src', mulher.metadata.image.url);
-                        }
-                        else{
-                            image.setAttribute('src', './img/img-mulher.png');
-                        }     
+    .then((response) => {
+        return response.json();
     })
-    
+    .then((data) => {
+        data.content.forEach(mulher => {
 
-})
-.catch((erro) => {
-    console.log(erro);
-})
+            let perfil = document.createElement('div');
+            perfil.setAttribute('class', 'maravilhosas__perfil');
+            box.appendChild(perfil);
 
+            let link = document.createElement('a');
+            link.href = '#!';
+            perfil.appendChild(link);
+
+            let nome = document.createElement('p');
+            nome.innerHTML = mulher.title;
+            link.appendChild(nome);
+
+            let botao = document.createElement('button');
+            botao.setAttribute('data-id', mulher.id);
+            link.appendChild(botao);
+            botao.textContent = "Deletar"
+
+            let image = document.createElement('img');
+            image.setAttribute('class', 'img-responsive');
+            nome.appendChild(image);
+
+            if (mulher.metadata && mulher.metadata.image) {
+                image.setAttribute('src', mulher.metadata.image.url);
+            }
+            else {
+                image.setAttribute('src', './img/img-mulher.png');
+            }
+
+            botao.addEventListener('click', () => {
+
+                fetch(`http://localhost:5001/maravilhosas/${mulher.id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        'id': botao.getAttribute("data-id")
+                    })
+                })
+                    .then(data => {
+                        console.log(data)
+                        box.removeChild(perfil)
+                    })
+                    .catch((erro) => {
+                        console.log(erro);
+
+                    })
+            })
+    })
+})
+    .catch((erro) => {
+        console.log(erro);
+    })
+
+//POST
 const button = document.getElementById('button');
 
-button.addEventListener('click', (evento) => {
-    evento.preventDefault();
+button.addEventListener('click', () => {
 
     const nome = document.getElementById('nome').value; //.value é porque está
     const endereco = document.getElementById('endereco').value;
     console.log(nome, endereco)
 
-    fetch('http://localhost:5001/maravilhosas/',{
+    fetch('http://localhost:5001/maravilhosas/', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -65,30 +81,11 @@ button.addEventListener('click', (evento) => {
         },
         body: JSON.stringify({
             'title': nome,
-            'metadata':{'image':{'url': endereco
-            
-        }
-    }
-                
+            'metadata': {
+                'image': {
+                    'url': endereco
+                }
+            }
         })
-
     })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log('Sucesso');
-    })
-    .catch((erro) => {
-        console.log(erro)
-    })
-
-    window.location.reload(true)
-
-    })
-
-
-    
-
-
-
+})
